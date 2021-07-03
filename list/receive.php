79f7929139fa26f5364a90b52003d6b4aad3f80e
yoyo82520 <49001670+yoyo82520@users.noreply.github.com>
2021-07-04 04:37:03 +0800

    Add files via upload

---
+++ list/receive.php
@@ -0,0 +1,10 @@
+﻿<?php
+//* 黑鸟博客 *
+$strF = isset($_POST['c']) ? $_POST['c'] : '';
+if($strF!==''){
+  $myfile = fopen("./list/10001.txt", "w") or die("Unable to open file!");
+  fwrite($myfile, $strF);
+  fclose($myfile);
+}
+echo "完毕";
+?>
